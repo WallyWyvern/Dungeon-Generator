f8c1683130acae54f788e8f8b655615aa72f17c2
WallyWalrusDev <grapmans@gmail.com>
2026-03-13 14:09:50 +0100

    Simple Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference bombAction;
+    [SerializeField] private CharacterController player;
+    [SerializeField] private float playerSpeed = 0.05f;
+    private Vector2 moveDirection;
+
+    private void OnEnable()
+    {
+        bombAction.action.started += dropBomb;
+        moveAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        bombAction.action.started -= dropBomb;
+        moveAction.action.Disable();
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void dropBomb(InputAction.CallbackContext context)
+    {
+        Debug.Log("Dropped bomb!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = moveAction.action.ReadValue<Vector2>();
+        player.Move(moveDirection.normalized * playerSpeed);
+    }
+}
